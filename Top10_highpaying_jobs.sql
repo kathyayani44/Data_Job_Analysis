73ad1a71d5a8bd01224c2241dd6caf0a07045fe0
kathyayani44 <kathyayani444@gmail.com>
2025-06-18 15:07:41 +0530

    Create Top10_highpaying_jobs.sql

---
+++ Top10_highpaying_jobs.sql
@@ -0,0 +1,14 @@
+/* Top 10 High Paying jobs */
+SELECT	
+	job_id,job_title,job_location,salary_year_avg,job_posted_date,name as company_name
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+	  job_location = 'Anywhere' AND 
+    job_title_short = 'Data Analyst' AND 
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
+/* Highest job paying role is Data Analyst of avg salary 650000 by Mantys Company */
